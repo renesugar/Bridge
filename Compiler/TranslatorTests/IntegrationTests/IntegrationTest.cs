3fd8391a1ad92e001e04f1329fa4bb1a9bd137fc
Danielku15 <danielku15@coderline.net>
2017-12-29 20:01:26 +0100

    Added dedicated integration tests for #3335

--- Compiler/TranslatorTests/IntegrationTests/IntegrationTest.cs
+++ Compiler/TranslatorTests/IntegrationTests/IntegrationTest.cs
@@ -105,6 +105,8 @@ namespace Bridge.Translator.Tests
 #endif
         [TestCase("20", false, true, TestName = "IntegrationTest 20 - Bridge.json autoProperty:plain")]
         [TestCase("22", false, true, TestName = "IntegrationTest 22 - Global Methods with nested classes")]
+        [TestCase("23", false, true, TestName = "IntegrationTest 23 - Module in bridge.json")]
+        [TestCase("24", false, true, TestName = "IntegrationTest 24 - Module as assembly attribute")]
         public void Test(string folder, bool isToBuild, bool useSpecialFileCompare, string markedContentFiles = null)
         {
             var logDir = Path.GetDirectoryName(Helpers.FileHelper.GetExecutingAssemblyPath());
---
+++ Compiler/TranslatorTests/TestProjects/23/Bridge/reference/TestProject.meta.js
@@ -0,0 +1,10 @@
+Bridge.assembly("TestProject", function ($asm, globals) {
+    "use strict";
+
+    define("MyModule", function () {
+        var MyModule = { };
+
+        return MyModule;
+    });
+
+});
